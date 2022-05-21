fd7d3a63dec444a1247d7586628f505827e0b4f8
danielnascimentotomaz <danielsabio1000@hotmail.com>
2022-05-21 14:23:24 -0300

    ANDAMENTO01

---
+++ src/aula/a1_Objetivo_Do_Curso.java
@@ -0,0 +1,28 @@
+/*
+*Objetivo do curso
+
+Ao final deste curso, o Dev será capaz de entender o
+que é o Collection Framework e a aplicabilidade de
+suas principais interfaces: List, Set e Map.
+Também aprenderá um pouco de Stream API e como
+ela pode nos ajudar na manipulação das collections.
+*
+*
+*Aula 1
+
+Coleções com iterações ordenadas: List
+
+Aula 2
+
+Coleções com Singularidade: Set
+
+Aula 3
+
+Coleções de Pares: Map
+
+Aula 4
+
+Stream API
+*
+*
+* */
